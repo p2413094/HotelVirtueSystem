e21748f9e45dc85cd31741a1220ce99d295c9098
p2413094 <p2413094@my365.dmu.ac.uk>
2020-02-13 12:48:54 +0000

    tstBookingCollection and clsBookingCollection added

---
+++ Hotel Virtue Testing/tstBookingCollection.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using HotelVirtueClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hotel_Virtue_Testing
+{
+    [TestClass]
+    public class tstBookingCollection
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsBookingCollection allBookings = new clsBookingCollection();
+            Assert.IsNotNull(allBookings);
+        }
+
+        [TestMethod]
+        public void BookingListOk()
+        {
+            clsBookingCollection allBookings = new clsBookingCollection();
+            List<clsBooking> testList = new List<clsBooking>();
+            clsBooking testItem = new clsBooking();
+            testItem.AdminId = 1;
+            testItem.ArrivalDate = DateTime.Now.Date;
+            testItem.BookingId = 1;
+            testItem.BookingLineId = 1;
+            testItem.CustomerId = 1;
+            testItem.DateTimeOfBooking = DateTime.Now;
+            testItem.Extras = "Extras";
+            testItem.HotelId = 1;
+            testItem.Other = "Other";
+            testItem.RoomId = 1;
+            testItem.SixteenUpwards = 1;
+            testItem.UnderFive = 1;
+            testItem.FiveToSixteen = 1;
+            testList.Add(testItem);
+            allBookings.BookingList = testList;
+            Assert.AreEqual(allBookings.BookingList, testList);
+        }
+
+        [TestMethod]
+        public void ThisBookingPropertyOk()
+        {
+            clsBookingCollection allBookings = new clsBookingCollection();
+            clsBooking testBooking = new clsBooking();
+            testBooking.AdminId = 1;
+            testBooking.ArrivalDate = DateTime.Now.Date;
+            testBooking.BookingId = 1;
+            testBooking.BookingLineId = 1;
+            testBooking.CustomerId = 1;
+            testBooking.DateTimeOfBooking = DateTime.Now;
+            testBooking.Extras = "Extras";
+            testBooking.HotelId = 1;
+            testBooking.Other = "Other";
+            testBooking.RoomId = 1;
+            testBooking.SixteenUpwards = 1;
+            testBooking.UnderFive = 1;
+            testBooking.FiveToSixteen = 1;
+            allBookings.ThisBooking = testBooking;
+            Assert.AreEqual(allBookings.ThisBooking, testBooking);
+        }
+
+        [TestMethod]
+        public void ListAndCountOk()
+        {
+            clsBookingCollection allBookings = new clsBookingCollection();
+            List<clsBooking> TestList = new List<clsBooking>();
+            clsBooking TestItem = new clsBooking();
+            TestItem.AdminId = 1;
+            TestItem.ArrivalDate = DateTime.Now.Date;
+            TestItem.BookingId = 1;
+            TestItem.BookingLineId = 1;
+            TestItem.CustomerId = 1;
+            TestItem.DateTimeOfBooking = DateTime.Now;
+            TestItem.DepartureDate = DateTime.Now.Date.AddDays(3);
+            TestItem.FiveToSixteen = 1;
+            TestItem.Other = "Other";
+            TestItem.RoomId = 1;
+            TestItem.SixteenUpwards = 1;
+            TestItem.UnderFive = 1;
+            TestList.Add(TestItem);
+            allBookings.BookingList = TestList;
+            Assert.AreEqual(allBookings.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void AddMethodOk()
+        {
+            clsBookingCollection allBookings = new clsBookingCollection();
+            clsBooking testItem = new clsBooking();
+            Int32 primaryKey = 0;
+            testItem.AdminId = 1;
+            testItem.ArrivalDate = DateTime.Now.Date;
+            testItem.BookingId = 1;
+            testItem.CustomerId = 1;
+            testItem.DateTimeOfBooking = DateTime.Now;
+            testItem.DepartureDate = DateTime.Now.Date.AddDays(3);
+            testItem.FiveToSixteen = 1;
+            testItem.Other = "Test2";
+            testItem.Extras = "Extras";
+            testItem.RoomId = 1;
+            testItem.SixteenUpwards = 1;
+            testItem.UnderFive = 1;
+            allBookings.ThisBooking = testItem;
+            primaryKey = allBookings.Add();
+            testItem.BookingLineId = primaryKey;
+
+            ////
+            //allBookings.ThisBooking.Find(primaryKey);
+            ////
+            Assert.AreEqual(allBookings.ThisBooking, testItem);
+        }
+    }
+}
---
+++ HotelVirtueClasses/clsBookingCollection.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelVirtueClasses
+{
+    public class clsBookingCollection
+    {
+        List<clsBooking> mBookingList = new List<clsBooking>();
+        clsBooking mThisBooking = new clsBooking();
+
+        public clsBookingCollection()
+        {
+            Int32 index = 0;
+            Int32 recordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblBookingLine_SelectAll");
+            recordCount = DB.Count;
+
+            while (index < recordCount)
+            {
+                clsBooking ABooking = new clsBooking();
+                ABooking.BookingLineId = Convert.ToInt32(DB.DataTable.Rows[index]["BookingLineId"]);
+                ABooking.BookingId = Convert.ToInt32(DB.DataTable.Rows[index]["BookingId"]);
+                ABooking.RoomId = Convert.ToInt32(DB.DataTable.Rows[index]["RoomId"]);
+                ABooking.UnderFive = Convert.ToInt32(DB.DataTable.Rows[index]["UnderFive"]);
+                ABooking.FiveToSixteen = Convert.ToInt32(DB.DataTable.Rows[index]["FiveToSixteen"]);
+                ABooking.SixteenUpwards = Convert.ToInt32(DB.DataTable.Rows[index]["SixteenUpwards"]);
+                ABooking.ArrivalDate = Convert.ToDateTime(DB.DataTable.Rows[index]["ArrivalDate"]);
+                ABooking.DepartureDate = Convert.ToDateTime(DB.DataTable.Rows[index]["DepartureDate"]);
+                ABooking.Extras = Convert.ToString(DB.DataTable.Rows[index]["Extras"]);
+                ABooking.Other = Convert.ToString(DB.DataTable.Rows[index]["Other"]);
+                mBookingList.Add(ABooking);
+                index++;
+            }
+        }
+
+        public List<clsBooking> BookingList
+        {
+            get { return mBookingList; }
+            set { mBookingList = value; }
+        }
+
+        public int Count
+        {
+            get { return mBookingList.Count; }
+            set { }
+        }
+        public clsBooking ThisBooking
+        {
+            get { return mThisBooking; }
+            set { mThisBooking = value; }
+        }
+
+        public int Add()
+        {
+
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("BookingId", mThisBooking.BookingId);
+            DB.AddParameter("RoomId", mThisBooking.RoomId);
+            DB.AddParameter("UnderFive", mThisBooking.UnderFive);
+            DB.AddParameter("FiveToSixteen", mThisBooking.FiveToSixteen);
+            DB.AddParameter("SixteenUpwards", mThisBooking.SixteenUpwards);
+            DB.AddParameter("ArrivalDate", mThisBooking.ArrivalDate);
+            DB.AddParameter("DepartureDate", mThisBooking.DepartureDate);
+            DB.AddParameter("Other", mThisBooking.Other);
+            DB.AddParameter("Extras", mThisBooking.Extras);
+            return DB.Execute("sproc_tblBookingLine_Insert");
+        }
+    }
+}
