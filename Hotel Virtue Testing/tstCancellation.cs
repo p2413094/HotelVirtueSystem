f44f6d6c2031f1fa20b6f62d1ebf04670202a433
rajeshdhooper <rajeshdhooper@icloud.com>
2020-02-14 22:25:10 +0000

    tstCancellation created

---
+++ Hotel Virtue Testing/tstCancellation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hotel_Virtue_Testing
+{
+    [TestClass]
+    public class tstCancellation
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
