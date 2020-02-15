284a69312afade89e9c265fbcffc81facb4d3b5d
rajeshdhooper <rajeshdhooper@icloud.com>
2020-02-15 17:55:14 +0000

    tst/ cls Cancellation added

--- Hotel Virtue Testing/tstCancellation.cs
+++ Hotel Virtue Testing/tstCancellation.cs
@@ -13,5 +13,104 @@ namespace Hotel_Virtue_Testing
             clsCancellation aCancellation = new clsCancellation();
             Assert.IsNotNull(aCancellation);
         }
+
+        [TestMethod]
+        public void CancellationIdPropertyOk()
+        {
+            clsCancellation aCancellation = new clsCancellation();
+            Int32 CancellationId = 1;
+            aCancellation.CancellationId = CancellationId;
+            Assert.AreEqual(aCancellation.CancellationId, CancellationId);
+        }
+
+        [TestMethod]
+        public void ReasonPropertyOk()
+        {
+            clsCancellation aCancellation = new clsCancellation();
+            string Reason = "Reason";
+            aCancellation.Reason = Reason;
+            Assert.AreEqual(aCancellation.Reason, Reason);
+        }
+
+        [TestMethod]
+        public void ReasonMinLessOne()
+        {
+            clsCancellation aCancellation = new clsCancellation();
+            string Error = "";
+            string reason = "";
+            Error = aCancellation.Valid(reason);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ReasonMinBoundary()
+        {
+            clsCancellation aCancellation = new clsCancellation();
+            string Error = "";
+            string reason = "a";
+            Error = aCancellation.Valid(reason);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ReasonMinPlusOne()
+        {
+            clsCancellation aCancellation = new clsCancellation();
+            string Error = "";
+            string reason = "ab";
+            Error = aCancellation.Valid(reason);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ReasonMaxLessOne()
+        {
+            clsCancellation aCancellation = new clsCancellation();
+            string Error = "";
+            string reason = "0123456789012345678901234567890123456789012345678";
+            Error = aCancellation.Valid(reason);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ReasonMaxBoundary()
+        {
+            clsCancellation aCancellation = new clsCancellation();
+            string Error = "";
+            string reason = "01234567890123456789012345678901234567890123456789";
+            Error = aCancellation.Valid(reason);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ReasonMaxPlusOne()
+        {
+            clsCancellation aCancellation = new clsCancellation();
+            string Error = "";
+            string reason = "012345678901234567890123456789012345678901234567890";
+            Error = aCancellation.Valid(reason);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ReasonMid()
+        {
+            clsCancellation aCancellation = new clsCancellation();
+            string Error = "";
+            string reason = "0123456789012345678901234";
+            Error = aCancellation.Valid(reason);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ReasonExtremeMax()
+        {
+            clsCancellation aCancellation = new clsCancellation();
+            string Error = "";
+            string reason = "";
+            reason = reason.PadRight(500, 'a');
+            Error = aCancellation.Valid(reason);
+            Assert.AreNotEqual(Error, "");
+        }
     }
 }
--- HotelVirtueClasses/clsCancellation.cs
+++ HotelVirtueClasses/clsCancellation.cs
@@ -1,6 +1,25 @@
-﻿namespace HotelVirtueClasses
+﻿using System;
+
+namespace HotelVirtueClasses
 {
     public class clsCancellation
     {
+        public int CancellationId { get; set; }
+        public int BookingLineId { get; set; }
+        public string Reason { get; set; }
+
+        public string Valid(string reason)
+        {
+            string Error = "";
+            if (reason.Length > 50)
+            {
+                Error = "Cancellation reason must not be longer than 50 characters";
+            }
+            if (reason.Length == 0)
+            {
+                Error = "Cancellation reason must not be blank";
+            }
+            return Error;
+        }
     }
 }
