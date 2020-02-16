cd8dbde50a27d775470412b6233a42ed1addd6f8
rajeshdhooper <rajeshdhooper@icloud.com>
2020-02-16 18:20:26 +0000

    tstPaymentCollection and clsPaymentCollection updated

---
+++ Hotel Virtue Testing/tstPaymentCollection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using HotelVirtueClasses;
+
+namespace Hotel_Virtue_Testing
+{
+    [TestClass]
+    public class tstPaymentCollection
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsPaymentColection AllPayments = new clsPaymentColection();
+            Assert.IsNotNull(AllPayments);
+        }
+
+        [TestMethod]
+        public void PaymentListOk()
+        {
+            clsPaymentColection AllPayments = new clsPaymentColection();
+            List<clsPayment> testList = new List<clsPayment>();
+            clsPayment testItem = new clsPayment();
+            testItem.Amount = 79.99m;
+            testItem.BookingLineId = 1;
+            testItem.CardNumber = "1234123412341234";
+            testItem.CardType = "VISA";
+            testItem.CustomerId = 1;
+            testItem.DateTimeOfPayment = DateTime.Now;
+            testItem.ExpiryDate = "07/21";
+            testItem.NameOnCard = "MR JOHN SMITH";
+            testItem.SecurityCode = "733";
+            testList.Add(testItem);
+            AllPayments.PaymentList = testList;
+            Assert.AreEqual(AllPayments.PaymentList, testList);
+        }   
+
+        [TestMethod]
+        public void ThisPaymentOk()
+        {
+            clsPaymentColection AllPayments = new clsPaymentColection();
+            clsPayment testPayment = new clsPayment();
+            testPayment.Amount = 79.99m;
+            testPayment.BookingLineId = 1;
+            testPayment.CardNumber = "1234123412341234";
+            testPayment.CardType = "VISA";
+            testPayment.CustomerId = 1;
+            testPayment.DateTimeOfPayment = DateTime.Now;
+            testPayment.ExpiryDate = "07/21";
+            testPayment.NameOnCard = "MR JOHN SMITH";
+            testPayment.SecurityCode = "733";
+            AllPayments.ThisPayment = testPayment;
+            Assert.AreEqual(AllPayments.ThisPayment, testPayment);
+        }
+
+        [TestMethod]
+        public void ListAndCountOk()
+        {
+            clsPaymentColection AllPayments = new clsPaymentColection();
+            List<clsPayment> testList = new List<clsPayment>();
+            clsPayment testItem = new clsPayment();
+            testItem.Amount = 79.99m;
+            testItem.BookingLineId = 1;
+            testItem.CardNumber = "1234123412341234";
+            testItem.CardType = "VISA";
+            testItem.CustomerId = 1;
+            testItem.DateTimeOfPayment = DateTime.Now;
+            testItem.ExpiryDate = "07/21";
+            testItem.NameOnCard = "MR JOHN SMITH";
+            testItem.SecurityCode = "733";
+            testList.Add(testItem);
+            AllPayments.PaymentList = testList;
+            Assert.AreEqual(AllPayments.Count, testList.Count);
+        }
+    }
+}
---
+++ HotelVirtueClasses/clsPaymentColection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace HotelVirtueClasses
+{
+    public class clsPaymentColection
+    {
+        List<clsPayment> mPaymentList = new List<clsPayment>();
+
+        public List<clsPayment> PaymentList
+        {
+            get { return mPaymentList; }
+            set { mPaymentList = value; }
+        }
+        public int Count
+        {
+            get { return mPaymentList.Count; }
+            set { }
+        }
+        public clsPayment ThisPayment { get; set; }
+
+        public clsPaymentColection()
+        {
+            Int32 index = 0;
+            Int32 recordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblPayment_SelectAll");
+            recordCount = DB.Count;
+            while (index < recordCount)
+            {
+                clsPayment aPayment = new clsPayment();
+                aPayment.Amount = Convert.ToDecimal(DB.DataTable.Rows[index]["Amount"]);
+                aPayment.BookingLineId = Convert.ToInt32(DB.DataTable.Rows[index]["BookingLineId"]);
+                aPayment.CardNumber = Convert.ToString(DB.DataTable.Rows[index]["CardNumber"]);
+                aPayment.CardType = Convert.ToString(DB.DataTable.Rows[index]["CardType"]);
+                aPayment.CustomerId = Convert.ToInt32(DB.DataTable.Rows[index]["CustomerId"]);
+                aPayment.DateTimeOfPayment = Convert.ToDateTime(DB.DataTable.Rows[index]["DateTimeOfPayment"]);
+                aPayment.ExpiryDate = Convert.ToString(DB.DataTable.Rows[index]["ExpiryDate"]);
+                aPayment.NameOnCard = Convert.ToString(DB.DataTable.Rows[index]["NameOnCard"]);
+                aPayment.PaymentId = Convert.ToInt32(DB.DataTable.Rows[index]["PaymentId"]);
+                aPayment.SecurityCode = Convert.ToString(DB.DataTable.Rows[index]["SecurityCode"]);
+                mPaymentList.Add(aPayment);
+                index++;
+            }
+        }
+    }
+}
