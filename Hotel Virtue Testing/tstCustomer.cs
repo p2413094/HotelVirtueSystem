2c05c652d2cc1648792f65b7f4126824e6f2a220
Michael babz <michaelbabalola79@gmail.com>
2020-02-21 20:42:39 +0000

    made a customer test

---
+++ Hotel Virtue Testing/tstCustomer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HotelVirtueClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hotel_Virtue_Testing
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsCustomer aCustomer = new clsCustomer();
+            Assert.IsNotNull(aCustomer);
+        }
+
+        [TestMethod]
+        public void CustomerIdPropertyOk()
+        {
+            clsCustomer aCustomer = new clsCustomer();
+            Int32 testData = 1;
+            aCustomer.CustomerId = testData;
+            Assert.AreEqual(aCustomer.CustomerId, testData);
+        }
+    }
+}
