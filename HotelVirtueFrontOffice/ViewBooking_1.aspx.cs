b745e0700c7d74c6e0504df7571f946391ff7d92
rajeshdhooper <rajeshdhooper@icloud.com>
2020-03-07 16:31:12 +0000

    Created drop down list for choosing booking that the user wishes to view

--- HotelVirtueFrontOffice/ViewBooking_1.aspx.cs
+++ HotelVirtueFrontOffice/ViewBooking_1.aspx.cs
@@ -8,9 +8,16 @@ using HotelVirtueClasses;
 
 public partial class ViewBooking_1 : System.Web.UI.Page
 {
+    DropDownList ddlBookingLineId = new DropDownList();
+    clsBookingCollection allBookings = new clsBookingCollection();
+    clsBookingLineCollection allBookingLines = new clsBookingLineCollection();
+
+    Int32 bookingLineId;
+
     protected void Page_Load(object sender, EventArgs e)
     {
         DisplayBookings();
+        CreateViewBookingSection();
     }
 
     protected void btnViewBooking_Click(object sender, EventArgs e)
@@ -27,13 +34,9 @@ public partial class ViewBooking_1 : System.Web.UI.Page
     {
         Response.Redirect("PayForBooking.aspx");
     }
-
-    
+   
     void DisplayBookings()
-    {
-        clsBookingCollection allBookings = new clsBookingCollection();
-        clsBookingLineCollection allBookingLines = new clsBookingLineCollection();
-
+    {     
         Int32 index = 0;
         Int32 recordCount = allBookings.BookingList.Count;
     
@@ -44,9 +47,9 @@ public partial class ViewBooking_1 : System.Web.UI.Page
             Panel pnlBooking = new Panel();
             pnlBooking.CssClass = "box";
 
-            Label lblBookingId = new Label();
-            lblBookingId.Text = "BookingId: " + bookingLine.BookingId;
-            pnlBooking.Controls.Add(lblBookingId);
+            Label lblBookingLineId = new Label();
+            lblBookingLineId.Text = "BookingLineId: " + bookingLine.BookingLineId;
+            pnlBooking.Controls.Add(lblBookingLineId);
             pnlBooking.Controls.Add(new LiteralControl("<br />"));
 
             Label lblCheckIn = new Label();
@@ -78,6 +81,44 @@ public partial class ViewBooking_1 : System.Web.UI.Page
             this.Controls.Add(new LiteralControl("<br />"));
 
             index++;           
+        }      
+    }
+
+    void CreateViewBookingSection()
+    {
+        Int32 index = 0;
+        Int32 recordCount = allBookingLines.BookingLineList.Count;
+
+        Panel pnlChooseBooking = new Panel();
+        pnlChooseBooking.CssClass = "box";
+
+        Label lblChooseBooking = new Label();
+        lblChooseBooking.Text = "Please choose the booking that you wish to view";
+        pnlChooseBooking.Controls.Add(lblChooseBooking);
+        pnlChooseBooking.Controls.Add(new LiteralControl("<br />"));
+
+        pnlChooseBooking.Controls.Add(new LiteralControl("<br />"));
+        pnlChooseBooking.Controls.Add(new LiteralControl("<br />"));
+
+        while (index < recordCount)
+        {
+            ddlBookingLineId.Items.Add((Convert.ToString(allBookingLines.BookingLineList[index].BookingLineId)));
+            index++;
         }
+
+        pnlChooseBooking.Controls.Add(ddlBookingLineId);
+        Form.Controls.Add(pnlChooseBooking);
+
+        Button btnViewThisBooking = new Button();
+        btnViewThisBooking.CssClass = "continueButton";
+        btnViewThisBooking.Text = "VIEW THIS BOOKING";
+        btnViewThisBooking.Click += BtnViewThisBooking_Click;
+        pnlChooseBooking.Controls.Add(btnViewThisBooking);
+        pnlChooseBooking.Controls.Add(new LiteralControl("<br />"));
+    }
+
+    private void BtnViewThisBooking_Click(object sender, EventArgs e)
+    {
+        bookingLineId = Convert.ToInt32(ddlBookingLineId.SelectedValue);
     }
 }
