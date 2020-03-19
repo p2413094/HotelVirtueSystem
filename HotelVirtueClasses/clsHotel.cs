61ef3871a802936ab4bdef01222f7d84cc1197f9
p2412923 <p2412923@my365.dmu.ac.uk>
2020-03-19 11:45:19 +0000

    done tstHotel

--- HotelVirtueClasses/clsHotel.cs
+++ HotelVirtueClasses/clsHotel.cs
@@ -1,6 +1,16 @@
-﻿namespace HotelVirtueClasses
+﻿using System;
+
+namespace HotelVirtueClasses
 {
     public class clsHotel
     {
+        public int HotelId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public DateTime DateofStay { get; set; }
+        public string Location { get; set; }
+
+
+
     }
 }
--- HotelVirtueClasses/clsHotelCollection.cs
+++ HotelVirtueClasses/clsHotelCollection.cs
@@ -1,4 +1,10 @@
-﻿namespace HotelVirtueClasses
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelVirtueClasses
 {
     public class clsHotelCollection
     {
